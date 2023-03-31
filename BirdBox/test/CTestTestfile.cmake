a632640e9d81dc55b3ddae09793300401ebe10e6
david <david2maykel@gmail.com>
2023-03-31 11:14:46 +0200

    clear

--- BirdBox/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lua/Bureau/projet-long/BirdBox/test
-# Build directory: /home/lua/Bureau/projet-long/BirdBox/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[BirdBoxTest]=] "/home/lua/Bureau/projet-long/BirdBox/test/BirdBoxTest")
-set_tests_properties([=[BirdBoxTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/lua/Bureau/projet-long/BirdBox/test/CMakeLists.txt;30;add_test;/home/lua/Bureau/projet-long/BirdBox/test/CMakeLists.txt;0;")
